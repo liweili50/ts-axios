15ffee6c3cfdb8d78afe55d69cd917abaaf59ece
liweili50 <liweili50@163.com>
2021-08-17 17:59:47 +0800

    feat(core & helpers & types): add new ways to axios call
    add object and function ways to use axios

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,6 +1,6 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
-import { parseHeaders } from './helpers/headers'
-import { createError } from './helpers/error'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types'
+import { parseHeaders } from '../helpers/headers'
+import { createError } from '../helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((reslove, reject) => {
@@ -16,7 +16,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       request.timeout = timeout
     }
 
-    request.open(method.toUpperCase(), url)
+    request.open(method.toUpperCase(), url!)
 
     request.onreadystatechange = function handleStateChange() {
       if (request.readyState !== 4) {
