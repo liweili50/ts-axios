fcf46079dc9ee2d6a07c346c2971324ad1206623
liweili50 <liweili50@163.com>
2021-08-25 09:18:28 +0800

    feat(axios): add static function to axios
    add static function to create axios instance

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,8 +22,8 @@ export interface AxiosRequestConfig {
   headers?: any
   responseType?: XMLHttpRequestResponseType
   timeout?: number
-  transformRequest: AxiosTransformer | AxiosTransformer[]
-  transformResponse: AxiosTransformer | AxiosTransformer[]
+  transformRequest?: AxiosTransformer | AxiosTransformer[]
+  transformResponse?: AxiosTransformer | AxiosTransformer[]
 
   [propName: string]: any
 }
@@ -69,6 +69,9 @@ export interface AxiosInstance extends Axios {
   <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
+export interface AxiosStatic extends AxiosInstance {
+  create(config?: AxiosRequestConfig): AxiosInstance
+}
 export interface AxiosInterceptorManager<T> {
   use(resolved: ResolvedFn<T>, rejectd?: RejectedFn): number
   eject(id: number): void
